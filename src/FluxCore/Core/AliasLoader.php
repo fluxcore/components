b154f8ebd8f8238484b26c58ff5709f6a4c88dd7
Kimmy Andersson <cyaspik@gmail.com>
2013-02-04 02:46:47 +0100

    AliasLoader

---
+++ src/FluxCore/Core/AliasLoader.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace FluxCore\Core;
+
+class AliasLoader
+{
+    private $aliasMap = array();
+
+    public function getAliasMap()
+    {
+        return $this->aliasMap;
+    }
+
+    /**
+     * @param array $aliasMap Class to filename map
+     */
+    public function addAliasMap(array $aliasMap)
+    {
+        if ($this->aliasMap) {
+            $this->aliasMap = array_merge($this->aliasMap, $aliasMap);
+        } else {
+            $this->aliasMap = $aliasMap;
+        }
+    }
+
+	/**
+	 * Registers this instance as an autoloader.
+	 *
+	 * @param bool $prepend Whether to prepend the autoloader or not
+	 */
+	public function register($prepend = true)
+	{
+		spl_autoload_register(array($this, 'loadClass'), true, $prepend);
+	}
+
+	/**
+	 * Unregisters this instance as an autoloader.
+	 */
+	public function unregister()
+	{
+		spl_autoload_unregister(array($this, 'loadClass'));
+	}
+
+	/**
+	 * Loads the given class or interface.
+	 *
+	 * @param  string	$class The name of the class
+	 * @return bool|null True, if loaded
+	 */
+	public function loadClass($class)
+	{
+		if (isset($this->aliasMap[$class])) {
+			class_alias($this->aliasMap[$class], $class);
+		}
+	}
+}
---
+++ tests/Core/AliasLoaderTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use FluxCore\Core\AliasLoader;
+use Composer\Autoload\ClassLoader;
+
+class AliasLoaderTest extends PHPUnit_Framework_TestCase
+{
+	public function setUp()
+	{
+		$this->loader = new AliasLoader();
+		$this->loader->register();
+	}
+
+	public function testFindFile()
+	{
+		$this->loader->addAliasMap(array('Test' => 'TestFile'));
+		$this->assertFalse(class_exists('ThisIsANonExistantClass'));
+		$this->assertTrue(class_exists('TestFile'));
+		$this->assertTrue(class_exists('Test'));
+		$this->assertFalse(class_exists('Testing'));
+	}
+}
+
+class TestFile
+{
+	public function hello()
+	{
+		return 'World';
+	}
+}
