31509cc49a1607c76057f107d7a9f01d7da0f1c3
Kimmy Andersson <cyaspik@gmail.com>
2013-02-04 11:57:33 +0100

    Facade
    Created a Facade base that proxies calls to an instance present in the
    Application IoC container.

---
+++ tests/Core/FacadeTest.php
@@ -0,0 +1,74 @@
+<?php
+
+use FluxCore\Core\Application;
+use FluxCore\Core\Facade;
+
+class FacadeTest extends PHPUnit_Framework_TestCase
+{
+	public function setUp()
+	{
+		$this->app = new Application;
+		Facade::setFacadeApplication($this->app);
+		Facade::clearFacadeResolvedInstances(); // I hate unit-testing static things.
+	}
+
+	public function testSetGetFacadeApplication()
+	{
+		$this->assertInstanceOf(
+			'FluxCore\Core\Application',
+			Facade::getFacadeApplication()
+		);
+	}
+
+	public function testGetFacadeObject()
+	{
+		$this->app['test'] = array('hello', 'world');
+		$this->assertEquals(
+			array('hello', 'world'),
+			FacadeStub::getFacadeObject()
+		);
+	}
+
+	public function testGetFacadeAccessorNotImplemented()
+	{
+		try {
+			FacadeStubNotImplemented::getFacadeAccessor();
+		} catch(RuntimeException $e) {
+			$this->assertEquals(
+				"Facade 'FacadeStubNotImplemented' does not implement getFacadeAccessor().",
+				$e->getMessage()
+			);
+
+			return;
+		}
+
+		$this->fail('Expected RuntimeException was not raised.');
+	}
+
+	public function testCallStaticProxy()
+	{
+		$this->app['test'] = new ClassStub();
+		$this->assertEquals(
+			'world',
+			FacadeStub::hello('world')
+		);
+	}
+}
+
+class FacadeStub extends Facade
+{
+	public static function getFacadeAccessor()
+	{
+		return 'test';
+	}
+}
+
+class FacadeStubNotImplemented extends Facade {}
+
+class ClassStub
+{
+	public function hello($world)
+	{
+		return $world;
+	}
+}
