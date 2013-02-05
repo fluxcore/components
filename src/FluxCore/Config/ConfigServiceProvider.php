1f5c09ea428ec764aff45e4071f3a3427eaf95f0
Kimmy Andersson <cyaspik@gmail.com>
2013-02-06 00:26:58 +0100

    Bugfix

--- src/FluxCore/Config/ConfigServiceProvider.php
+++ src/FluxCore/Config/ConfigServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace FluxCore\Config;
 
+use FluxCore\Config\Engine\PhpEngine;
 use FluxCore\Core\Service\ServiceProvider;
 use FluxCore\IO\FileFinder;
 
@@ -9,10 +10,18 @@ class ConfigServiceProvider extends ServiceProvider
 {
 	public function register()
 	{
+		$this->app['config.resolver'] = $this->app->share(function($app)
+		{
+			$resolver = new EngineResolver();
+			$resolver->add('php', new PhpEngine());
+
+			return $resolver;
+		});
+
 		$this->app['config'] = $this->app->share(function($app)
 		{
 			return new ConfigManager(
-				new EngineResolver(),
+				$app['config.resolver'],
 				new FileFinder($app['config.path'])
 			);
 		});
