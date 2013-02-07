4cccaa24d349a76c3fe4cd38b55ffadd69b48736
Kimmy Andersson <cyaspik@gmail.com>
2013-02-07 17:01:41 +0100

    Route

---
+++ src/FluxCore/Routing/Route.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace FluxCore\Routing;
+
+class Route
+{
+	protected $pattern;
+	protected $method;
+	protected $handler;
+
+	function __construct($pattern, $method, \Closure $handler)
+	{
+		$this->setPattern($pattern);
+		$this->setMethod($method);
+		$this->setHandler($handler);
+	}
+
+	public function getKey()
+	{
+		return md5("{$this->method}:{$this->pattern}");
+	}
+
+	public function getPattern()
+	{
+		return $this->pattern;
+	}
+
+	public function setPattern($pattern)
+	{
+		$pattern = trim($pattern, '/');
+		$pattern = ($pattern != '') ? $pattern : '/';
+
+		return ($this->pattern = $pattern);
+	}
+
+	public function getMethod()
+	{
+		return $this->method;
+	}
+
+	public function setMethod($method)
+	{
+		$method = strtoupper($method);
+
+		return ($this->method = $method);
+	}
+
+	public function getHandler()
+	{
+		return $this->handler;
+	}
+
+	public function setHandler(\Closure $handler)
+	{
+		return ($this->handler = $handler);
+	}
+
+	public function dispatch($arguments = array())
+	{
+		return closure_proxy($this->handler, $arguments);
+	}
+}
---
+++ tests/Routing/RouteTest.php
@@ -0,0 +1,56 @@
+<?php
+
+use FluxCore\Routing\Route;
+
+class RouteTest extends PHPUnit_Framework_TestCase
+{
+	public function setUp()
+	{
+		$this->route = new Route('', '', function() {});
+	}
+
+	public function testPattern()
+	{
+		$this->route->setPattern('/');
+		$this->assertEquals('/', $this->route->getPattern());
+
+		$this->route->setPattern('');
+		$this->assertEquals('/', $this->route->getPattern());
+
+		$this->route->setPattern('/test/hello/');
+		$this->assertEquals('test/hello', $this->route->getPattern());
+
+		$this->route->setPattern('/test/');
+		$this->assertEquals('test', $this->route->getPattern());
+	}
+
+	public function testMethod()
+	{
+		$this->route->setMethod('get');
+		$this->assertEquals('GET', $this->route->getMethod());
+
+		$this->route->setMethod('post');
+		$this->assertEquals('POST', $this->route->getMethod());
+	}
+
+	public function testKey()
+	{
+		$this->route->setMethod('GET');
+		$this->route->setPattern('hello/world');
+
+		$this->assertEquals(md5("GET:hello/world"), $this->route->getKey());
+	}
+
+	public function testHandlerDispatch()
+	{
+		$this->route->setHandler(function($name)
+		{
+			return "Hello $name.";
+		});
+
+		$this->assertEquals(
+			'Hello John.',
+			$this->route->dispatch(array('John'))
+		);
+	}
+}
