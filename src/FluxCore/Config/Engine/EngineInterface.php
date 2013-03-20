84099c26b1a30c8e877ac2703652f865c3409cd1
Kimmy Andersson <cyaspik@gmail.com>
2013-03-20 09:42:52 +0100

    Forgot to PHPDoc these.

--- src/FluxCore/Config/Engine/EngineInterface.php
+++ src/FluxCore/Config/Engine/EngineInterface.php
@@ -2,7 +2,17 @@
 
 namespace FluxCore\Config\Engine;
 
+/**
+ * Describes a configuration engine interface.
+ */
 interface EngineInterface
 {
+	/**
+	 * Parses the specified file into a readable
+	 * configuration object.
+	 * 
+	 * @param string $file
+	 * @return FluxCore\Config\Configuration
+	 */
 	public function parse($file);
 }
--- src/FluxCore/Config/Engine/PhpEngine.php
+++ src/FluxCore/Config/Engine/PhpEngine.php
@@ -4,8 +4,20 @@ namespace FluxCore\Config\Engine;
 
 use FluxCore\Config\Configuration;
 
+/**
+ * PHP configuration engine class.
+ *
+ * PHP file parser engine for configuration.
+ */
 class PhpEngine implements EngineInterface
 {
+	/**
+	 * Parses the specified PHP file into
+	 * a readable configuration object.
+	 * 
+	 * @param string $file
+	 * @return FluxCore\Config\Configuration
+	 */
 	public function parse($file)
 	{
 		return new Configuration(include $file);
